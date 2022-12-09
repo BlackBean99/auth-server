550611957c29372a07eab71889e218744f893822
unknown <ymecca12@gmail.com>
2022-12-09 22:23:12 +0900

    dev : auth-cloud msa 환경 구축

---
+++ src/auth-cloud/src/main/java/com/smilegate/authcloud/AuthcloudApplication.java
@@ -0,0 +1,13 @@
+package com.smilegate.authcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcloudApplication.class, args);
+	}
+
+}
